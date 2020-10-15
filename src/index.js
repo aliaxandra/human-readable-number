c8b4b80ef4b391ba718d6fc68809f12ccdb0429f
Alexandra <aliaxandra@gmail.com>
2020-10-15 13:06:40 +0300

    init: start human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function toReadable (number) {
-  
+    let a = ['zero', 'one', 'two', 'three', 'four',
+        'five', 'six', 'seven', 'eight', 'nine'];
+    let b = ['ten', 'eleven', 'twelve', 'thirteen',
+        'fourteen', 'fifteen', 'sixteen', 'seventeen',
+        'eighteen', 'nineteen']
+    let c = ['twenty', 'thirty', 'forty', 'fifty',
+        'sixty', 'seventy', 'eighty', 'ninety'];
+
+    
 }
